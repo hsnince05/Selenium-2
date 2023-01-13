68edef40fdfdc7b795d319b8e22f8d6f46fd013b
Hasan INCE <hsnc0505@gmail.com>
2023-01-13 23:52:41 +0300

    13/01/2022 Day14

--- src/test/java/homework/WindowHandle.java
+++ src/test/java/homework/WindowHandle.java
@@ -1,5 +1,6 @@
 package homework;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
@@ -10,24 +11,46 @@ import java.util.List;
 
 public class WindowHandle extends TestBase {
 
+
     @Test
-    public void test01(){
+    public void test01() {
         //Test01 :  1- amazon gidin
-        driver.get("https://amazon.com");
+        driver.get("https://www.amazon.com");
 
         //Arama kutusunun solundaki dropdown menuyu handle edip listesini ekrana yazdırın
-        WebElement iframe= driver.findElement(By.xpath("//*[@id='nav-search-dropdown-card']"));
+        WebElement iframe = driver.findElement(By.xpath("//*[@id='searchDropdownBox']"));
 
         Select select = new Select(iframe);
-        List<WebElement>ddm = select.getOptions();
-        System.out.println(ddm);
+        List<WebElement> ddm = select.getOptions();
+        ddm.stream().forEach(t -> System.out.println(t.getText()));
 
+        System.out.println(ddm.size());
         //dropdown menude 40 eleman olduğunu doğrulayın
+        Assert.assertFalse(ddm.size() == 40);
+    }
+        @Test
+        public void test02(){
         //Test02
         //dropdown menuden elektronik bölümü seçin
+            WebElement iframe = driver.findElement(By.xpath("//*[@id='searchDropdownBox']"));
+
+            Select select = new Select(iframe);
+            select.selectByVisibleText("Electronics");
+
         //arama kutusuna iphone yazip aratin ve bulunan sonuç sayısını yazdırın
+            driver.findElement(By.xpath("//*[@id='twotabsearchtextbox']")).sendKeys("iphone");
+
         //sonuc sayisi bildiren yazinin iphone icerdigini test edin
+            Assert.assertTrue(driver.findElement(By.xpath("//*[@class='a-section a-spacing-small a-spacing-top-small']")).
+                    getText().contains("iphone"));
+
         //ikinci ürüne relative locater kullanarak tıklayin
+        WebElement birinciUrun=driver.findElement(By.xpath("//*[@data-asin='B0BG875KCH']"));
+        WebElement ucuncuUrun=driver.findElement(By.xpath("//*[@data-asin='B0BP73DD5S']"));
+
+        //driver.findElement()
+
+
         //ürünün title'ni ve fiyatını variable’a  assign edip ürünü sepete ekleyelim
         //Test03
         //yeni bir sekme açarak amazon anasayfaya gidin
