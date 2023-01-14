bd7c8d3cfeade7aad70ea4658b58ca3239f4d318
Hasan INCE <hsnc0505@gmail.com>
2023-01-14 23:53:14 +0300

    14/01/2022 Day15

--- src/test/java/day15/C01_ExtentReports2.java
+++ src/test/java/day15/C01_ExtentReports2.java
@@ -1,26 +1,35 @@
-package day14;
+package day15;
+
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
-import org.openqa.selenium.By;
 import utilities.TestBase;
+
 import java.text.SimpleDateFormat;
 import java.util.Date;
-public class C03_ExtentReports extends TestBase {
+
+public class C01_ExtentReports2 extends TestBase {
     //    HATIRLAMAMIZ GEREKEN 3 CLASS
     protected static ExtentReports extentReports;
     protected static ExtentHtmlReporter extentHtmlReporter;
     protected static ExtentTest extentTest;
-    @Test
-    public void extentReportsTest(){
+
+
+    @BeforeClass
+    public static void extentReportsSetUp(){
+
+
 //      REPORT  PATH
         String currentTime = new SimpleDateFormat("yyyyMMddhhmmss").format(new Date());
         String path = System.getProperty("user.dir")+"/test-output/reports/"+currentTime+"html_report.html";
 
 //        creating HTML report in the path
-        extentHtmlReporter = new ExtentHtmlReporter(path);
 
+
+        extentHtmlReporter = new ExtentHtmlReporter(path);
 //        creating extent reports object for generating the Entire reports with configuration
         extentReports = new ExtentReports();
 
@@ -41,29 +50,29 @@ public class C03_ExtentReports extends TestBase {
 
 
 //        ********************************RAPOR AYARLARI BITTI****************************
+
 //        Raporu projeme ekliyorum
         extentReports.attachReporter(extentHtmlReporter);
 
-
-        // Extent Test objesini olustur
+//        Extent Test objesi ni olustur
         extentTest = extentReports.createTest("Extent Report Login Test","Smoke Test Raporu");
-
-
-        //TUM AYARLAR BITTI EXTENT TEST OBJESI ILE LOGLAMA(RAPORA YAZDIRMA) ISLEMINI YAPABILIRIM
-        extentTest.pass("Kullanici ana sayfaya gider");
-        driver.get("https://www.techproeducation.com");
-
-
-        //LMS SAYFASİNA GİDELİM
-        extentTest.pass("Kullanici LMS sayfasina gider");
-        driver.findElement(By.linkText("LMS LOGIN")).click();
-
-        //TEST BITTI
-        extentTest.pass("TEST BASARIYLA GERCEKLESTI");
-
-
-        //RAPORU GOSTER. RAPORUN OLUSMASI ICIN BU ADIM ZORUNLUDUR
-        extentReports.flush();
-
     }
+
+
+    @Test
+    public void extentReportsTest(){
+        extentTest.pass("PASS");
+        extentTest.info("BILGILENDIRME NOTU");
+        extentTest.fail("FAILED");
+        extentTest.warning("UYARI MESAJI");
+        extentTest.skip("ATLAMA MESAJI");
+        extentTest.fatal("COKUS HATASI");
+    }
+
+
+    @AfterClass
+    public static void extentReportsTearDown(){
+        extentReports.flush();
+    }
+
 }
