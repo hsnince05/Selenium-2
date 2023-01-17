ff0f797d19b62336af8091e75980d2e048e211f3
Hasan INCE <hsnc0505@gmail.com>
2023-01-18 00:54:49 +0300

    17/01/2022 Practice

--- src/test/java/homework/day11_Challenge_MarketTask.java
+++ src/test/java/homework/day11_Challenge_MarketTask.java
@@ -2,7 +2,11 @@ package homework;
 
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
 import utilities.TestBase;
+
+import java.util.List;
 
 public class day11_Challenge_MarketTask extends TestBase {
 
@@ -12,10 +16,23 @@ public class day11_Challenge_MarketTask extends TestBase {
         driver.get("https://www.amazon.com.tr/");
 
         //Search iPhone13 512
-        driver.findElement(By.xpath(""));
+        driver.findElement(By.xpath("//*[@id='twotabsearchtextbox']")).sendKeys("iPhone13 512", Keys.ENTER);
+
         //Check that the results are listed
+        List<WebElement> iphone = driver.findElements(By.xpath("//*[@class='s-image']"));
+        iphone.stream().forEach(t-> System.out.println(t.getText()));
+
+        waitFor(2);
         //Click iPhone13 at the top of the list
+
+        driver.findElement(By.xpath("(//*[@class='s-image'])[1]")).click();
+        waitFor(2);
+
         //Log the following values for each size .Size information .Price .Color .Stock status
+        String isim = driver.findElement(By.xpath("//*[@id='productTitle']")).getText();
+        System.out.println(isim);
+
+
 
 
     }
--- src/test/java/practice/practice03/P07_Actions.java
+++ src/test/java/practice/practice03/P07_Actions.java
@@ -1,6 +1,10 @@
 package practice.practice03;
 
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import utilities.TestBase;
 
 public class P07_Actions extends TestBase {
@@ -9,5 +13,33 @@ public class P07_Actions extends TestBase {
     - Çıkan ekrana istediğiniz çizgi yada şekli çiziniz
     - Son olarak sayfayı temizleyiniz
      */
+
+    @Test
+    public void test01(){
+
+        driver.get("http://szimek.github.io/signature_pad/");
+        WebElement  aa = driver.findElement(By.xpath("//canvas"));
+        Actions actions = new Actions(driver);
+        actions.moveToElement(aa).clickAndHold();
+
+        for (int  i = 0 ; i<10 ; i++){
+            actions.moveByOffset(-5,-5);
+        }
+
+
+        for (int  i = 0 ; i<10 ; i++){
+            actions.moveByOffset(0,5);
+        }
+
+        for (int  i = 0 ; i<10 ; i++){
+            actions.moveByOffset(5,0);
+        }
+        actions.release().build().perform();
+
+        waitFor(3);
+
+        driver.findElement(By.xpath("//*[.='Clear']")).click();
+
+    }
 
 }
